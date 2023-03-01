bafc87274dd09e1945e82435ed29f1129792f8b3
Tom Clive <tomclive@Toms-MacBook-Pro.local>
2023-03-01 11:09:30 +0000

    Enable collab legislation index to parse html

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,6 +14,7 @@ set :deploy_to, deploysecret(:deploy_to)
 set :ssh_options, port: deploysecret(:ssh_port)
 
 set :repo_url, "https://github.com/CoslaDigital/Consul"
+set :branch, "develop"
 
 set :revision, `git rev-parse --short #{fetch(:branch)}`.strip
 
