fc393365580823c9d448557ab5a37ba545a160e2
mattix7771 <mattione7@gmail.com>
2023-04-25 17:08:57 +0100

    Moved rake task to controller

--- app/controllers/admin/postcodes_controller.rb
+++ app/controllers/admin/postcodes_controller.rb
@@ -1,3 +1,6 @@
+require 'rexml/document'
+require 'rexml/xpath'
+
 class Admin::PostcodesController < Admin::BaseController
   respond_to :html
 
@@ -22,9 +25,37 @@ class Admin::PostcodesController < Admin::BaseController
     uploaded_file = params[:file]
     file_path = uploaded_file.path
 
-    ENV['FILE_PATH'] = file_path
-    system('rake postcodes:import_csv')
+    postcode_validation(file_path)
+
     redirect_to admin_postcodes_path
+  end
+
+  def postcode_validation(file_path)
+
+    # Load the XML file containing the postal code regex patterns for each territory
+    xml_file = File.new("lib/tasks/postcodes.xml")
+    xml_doc = REXML::Document.new(xml_file)
+
+    # Build a hash of territory codes and regex patterns
+    regex_patterns = {}
+    REXML::XPath.each(xml_doc, "//postCodeRegex") do |element|
+      territory_id = element.attributes["territoryId"]
+      regex_patterns[territory_id] = Regexp.new(element.text)
+    end
+
+    CSV.foreach(file_path, headers: true) do |row| 
+      next unless row["ward"].present? && row["postcode"].present? #rejects empty entries
+
+      p row
+      ward = "init"
+	    ward = row["ward"].strip
+      postcode = row["postcode"].delete(' ').upcase #deletes empty spaces and upper case
+
+      if regex_patterns.any? { |_, pattern| postcode =~ pattern }  && postcode.length <= 10
+        geozone = Geozone.find_or_create_by!(name: ward)
+        Postcode.create!(postcode:postcode, ward:ward, geozone_id:geozone.id) unless Postcode.exists?(postcode: postcode) #rejects duplicate postcodes
+      end
+    end
   end
 
   def edit
